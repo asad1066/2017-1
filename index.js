846e60fd75576521ff2b12b1f318b7f1dd1f7d81
William Sadler <andysadler@seattleacademy.org>
2020-02-05 09:33:17 -0800

    Add server pages

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log("hello world")
+const express = require('express')
+const app = express()
+const port = 1501
+let counter = 0 
+
+app.use(express.static("public"));
+
+app.get('/', (req, res) => res.send('Hello Andy'))
+
+app.get('/counter', function (req, res) {
+	counter = counter + 1; 
+  res.send('counter ' + counter);
+})
+
+app.get('/date', function (req, res) {
+	let d = new Date();
+	console.log(d);
+  res.send('Date ' + d);
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
